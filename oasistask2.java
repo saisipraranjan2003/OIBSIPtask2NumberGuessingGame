0fa7cb95e4e27830ad429d3bc8c2f41a65ac0c59
SAI SIPRARANJAN <140781380+saisipraranjan2003@users.noreply.github.com>
2023-07-28 10:41:20 +0530

    Update oasistask2.java

--- oasistask2.java
+++ oasistask2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class oasistask1 {
+public class oasistask2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
